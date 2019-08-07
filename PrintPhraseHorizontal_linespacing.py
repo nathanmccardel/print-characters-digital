e709a4bd6df2bb2ef2aa96954706d3d12482b9a8
Nathan McCardel <natemccardel@gmail.com>
2019-08-07 14:31:56 -0500

    Add files via upload

---
+++ PrintPhraseHorizontal_linespacing.py
@@ -0,0 +1,547 @@
+phrase=str(input("Enter a phrase: "))
+
+#for c in phrase:
+#    print(c)
+
+#need to add in lower case letters?
+
+adict = {
+    0:"     *    ",
+    1:"    * *   ",
+    2:"   *   *  ",
+    3:"  *     * ",
+    4:" ******** ",
+    5:" *      * ",
+    6:" *      * ",
+    }
+
+bdict = {
+    0:"********  ",
+    1:"*       **",
+    2:"*      ** ",
+    3:"*******   ",
+    4:"*      ** ",
+    5:"*       **",
+    6:"********  ",
+    }
+
+cdict = {
+    0:"   *****  ",
+    1:"  *     * ",
+    2:" *        ",
+    3:"*         ",
+    4:" *        ",
+    5:"  *     * ",
+    6:"   *****  ",
+    }
+
+ddict = {
+    0:"*******   ",
+    1:"*       * ",
+    2:"*        *",
+    3:"*        *",
+    4:"*        *",
+    5:"*       * ",
+    6:"*******   ",
+    }
+
+edict = {
+    0:"**********",
+    1:"*         ",
+    2:"*         ",
+    3:"*******   ",
+    4:"*         ",
+    5:"*         ",
+    6:"**********",
+    }
+
+fdict = {
+    0:"**********",
+    1:"*         ",
+    2:"*         ",
+    3:"*******   ",
+    4:"*         ",
+    5:"*         ",
+    6:"*         ",
+    }
+
+gdict = {
+    0:"   *****  ",
+    1:"  *     * ",
+    2:" *        ",
+    3:"*         ",
+    4:" *    ****",
+    5:"  *     * ",
+    6:"   *****  ",
+    }
+
+hdict = {
+    0:"*        *",
+    1:"*        *",
+    2:"*        *",
+    3:"**********",
+    4:"*        *",
+    5:"*        *",
+    6:"*        *",
+    }
+
+idict = {
+    0:"**********",
+    1:"    *     ",
+    2:"    *     ",
+    3:"    *     ",
+    4:"    *     ",
+    5:"    *     ",
+    6:"**********",
+    }
+
+
+
+jdict = {
+    0:"**********",
+    1:"    *     ",
+    2:"    *     ",
+    3:"    *     ",
+    4:"*   *     ",
+    5:" *  *     ",
+    6:"  **      ",
+    }
+
+
+
+kdict = {
+    0:"*       **",
+    1:"*     **  ",
+    2:"*   **    ",
+    3:"****      ",
+    4:"*   **    ",
+    5:"*     **  ",
+    6:"*       **",
+    }
+
+
+
+ldict = {
+    0:"*         ",
+    1:"*         ",
+    2:"*         ",
+    3:"*         ",
+    4:"*         ",
+    5:"*         ",
+    6:"**********",
+    }
+
+
+
+mdict = {
+    0:"**      **",
+    1:"* **  ** *",
+    2:"*   **   *",
+    3:"*   **   *",
+    4:"*        *",
+    5:"*        *",
+    6:"*        *",
+    }
+
+
+
+ndict = {
+    0:"**       *",
+    1:"* *      *",
+    2:"*  *     *",
+    3:"*    *   *",
+    4:"*     *  *",
+    5:"*      * *",
+    6:"*       **",
+    }
+
+
+
+odict = {
+    0:"   ****   ",
+    1:"  *    *  ",
+    2:" *      * ",
+    3:"*        *",
+    4:" *      * ",
+    5:"  *    *  ",
+    6:"   ****   ",
+    }
+
+
+
+pdict = {
+    0:"********  ",
+    1:"*       * ",
+    2:"*       * ",
+    3:"********  ",
+    4:"*         ",
+    5:"*         ",
+    6:"*         ",
+    }
+
+
+
+qdict = {
+    0:"   ****   ",
+    1:"  *    *  ",
+    2:" *      * ",
+    3:"*    *   *",
+    4:" *    * * ",
+    5:"  *    *  ",
+    6:"   **** * ",
+    }
+
+
+
+rdict = {
+    0:"********  ",
+    1:"*       * ",
+    2:"*       * ",
+    3:"********  ",
+    4:"* **      ",
+    5:"*   **    ",
+    6:"*     *** ",
+    }
+
+
+
+sdict = {
+    0:"  ******  ",
+    1:" *      * ",
+    2:"  *       ",
+    3:"     *    ",
+    4:"       *  ",
+    5:" *      * ",
+    6:"  ******  ",
+    }
+
+
+
+tdict = {
+    0:"**********",
+    1:"    *     ",
+    2:"    *     ",
+    3:"    *     ",
+    4:"    *     ",
+    5:"    *     ",
+    6:"    *     ",
+    }
+
+
+
+udict = {
+    0:"*        *",
+    1:"*        *",
+    2:"*        *",
+    3:"*        *",
+    4:"*        *",
+    5:" *      * ",
+    6:"  ******  ",
+    }
+
+
+
+vdict = {
+    0:"*       * ",
+    1:"*       * ",
+    2:" *     *  ",
+    3:" *     *  ",
+    4:"  *   *   ",
+    5:"   * *    ",
+    6:"    *     ",
+    }
+
+
+
+wdict = {
+    0:"*        *",
+    1:"*        *",
+    2:"*        *",
+    3:"*        *",
+    4:" *  **  * ",
+    5:" * *  * * ",
+    6:"  *    *  ",
+    }
+
+
+
+xdict = {
+    0:"*        *",
+    1:" *      * ",
+    2:"  *    *  ",
+    3:"   ***    ",
+    4:"  *   *   ",
+    5:" *      * ",
+    6:"*        *",
+    }
+
+
+
+
+ydict = {
+    0:"**      **",
+    1:"  *   *   ",
+    2:"   * *    ",
+    3:"    *     ",
+    4:"    *     ",
+    5:"    *     ",
+    6:"    *     ",
+    }
+
+
+
+zdict = {
+    0:"**********",
+    1:"        * ",
+    2:"      *   ",
+    3:"    *     ",
+    4:"  *       ",
+    5:" *        ",
+    6:"**********",
+    }
+
+spacedict = {
+    0:"          ",
+    1:"          ",
+    2:"          ",
+    3:"          ",
+    4:"          ",
+    5:"          ",
+    6:"          ",
+    }
+
+perioddict = {
+    0:"     ",
+    1:"     ",
+    2:"     ",
+    3:"     ",
+    4:"     ",
+    5:" *** ",
+    6:" *** ",
+    }
+
+commadict = {
+    0:"     ",
+    1:"     ",
+    2:"     ",
+    3:"     ",
+    4:" *** ",
+    5:"  ** ",
+    6:" **  ",
+    }
+
+apostrophedict = {
+    0:" *** ",
+    1:"  ** ",
+    2:" **  ",
+    3:"     ",
+    4:"     ",
+    5:"     ",
+    6:"     ",
+    }
+	
+colondict = {
+    0:" *** ",
+    1:" *** ",
+    2:"     ",
+    3:"     ",
+    4:"     ",
+    5:" *** ",
+    6:" *** ",
+    }
+	
+semicolondict = {
+    0:" *** ",
+    1:" *** ",
+    2:"     ",
+    3:"     ",
+    4:" *** ",
+    5:"  ** ",
+    6:" **  ",
+    }
+
+exclamationdict = {
+    0:"**** ",
+    1:" **  ",
+    2:" **  ",
+    3:" **  ",
+    4:"     ",
+    5:" **  ",
+    6:" **  ",
+    }
+
+questiondict = {
+    0:" *** ",
+    1:"*  **",
+    2:"  ** ",
+    3:" **  ",
+    4:"     ",
+    5:" **  ",
+    6:" **  ",
+    }
+	
+leftparendict = {
+    0:"  *  ",
+    1:" *   ",
+    2:"*    ",
+    3:"*    ",
+    4:"*    ",
+    5:" *   ",
+    6:"  *  ",
+    }
+
+rightparendict = {
+    0:"  *  ",
+    1:"   * ",
+    2:"    *",
+    3:"    *",
+    4:"    *",
+    5:"   * ",
+    6:"  *  ",
+    }
+
+dictdict = {
+    "a": adict,
+    "b": bdict,
+    "c": cdict,
+    "d": ddict,
+    "e": edict,
+    "f": fdict,
+    "g": gdict,
+    "h": hdict,
+    "i": idict,
+    "j": jdict,
+    "k": kdict,
+    "l": ldict,
+    "m": mdict,
+    "n": ndict,
+    "o": odict,
+    "p": pdict,
+    "q": qdict,
+    "r": rdict,
+    "s": sdict,
+    "t": tdict,
+    "u": udict,
+    "v": vdict,
+    "w": wdict,
+    "x": xdict,
+    "y": ydict,
+    "z": zdict,
+    "A": adict,
+    "B": bdict,
+    "C": cdict,
+    "D": ddict,
+    "E": edict,
+    "F": fdict,
+    "G": gdict,
+    "H": hdict,
+    "I": idict,
+    "J": jdict,
+    "K": kdict,
+    "L": ldict,
+    "M": mdict,
+    "N": ndict,
+    "O": odict,
+    "P": pdict,
+    "Q": qdict,
+    "R": rdict,
+    "S": sdict,
+    "T": tdict,
+    "U": udict,
+    "V": vdict,
+    "W": wdict,
+    "X": xdict,
+    "Y": ydict,
+    "Z": zdict,
+    " ": spacedict,
+    ".": perioddict,
+    ",": commadict,
+    "'": apostrophedict,
+    "!": exclamationdict,
+    "?": questiondict,
+    ":": colondict,
+    ";": semicolondict,
+    "(": leftparendict,
+    ")": rightparendict
+    
+    }
+
+#create another loop that measures length of phrase, skips to new set of lines past certain point
+#like
+#initialize
+potentialspaceindex = 0
+linelength = 4
+
+def runtheprint(potentialspaceindex):
+    if potentialspaceindex > 0:
+        potentialspaceindex = potentialspaceindex + 1
+    startindex = potentialspaceindex
+    if potentialspaceindex + linelength > len(phrase):
+        lastindex = len(phrase)
+        potentialspaceindex = lastindex
+        if phrase[startindex] == " ":
+            startindex = startindex + 1
+    else:
+        lastindex = potentialspaceindex+linelength
+        for k in range(potentialspaceindex, lastindex):
+            currentchar = phrase[k]
+            if currentchar == " ":
+                potentialspaceindex = k
+        if potentialspaceindex == startindex:
+            if startindex == 0:
+                potentialspaceindex = lastindex
+            else:
+                potentialspaceindex = lastindex - 1
+                startindex = startindex - 1
+
+    for j in range(0,7):
+        line = ""
+        for i in range(startindex,potentialspaceindex):
+            charcurrent = phrase[i]
+            line = line + " " + dictdict[charcurrent][j]
+        print(line)
+    if lastindex < len(phrase):
+        runtheprint(potentialspaceindex)
+
+
+#next try
+potentialspaceindex = 0
+linelength = 10
+def runtheprint2(potentialspaceindex):
+    startindex = potentialspaceindex
+    if potentialspaceindex + linelength >= len(phrase):
+        lastindex = len(phrase)
+        potentialspaceindex = lastindex
+        if phrase[startindex] == " ":
+            startindex = startindex + 1
+    else:
+        lastindex = potentialspaceindex+linelength
+        if phrase[lastindex] == " ":
+            potentialspaceindex = potentialspaceindex
+        else:
+            if phrase[startindex] == " ":
+                potentialspaceindex = potentialspaceindex + 1
+                startindex = startindex + 1
+      #          print("otherthing",end='\n')
+            for k in range(potentialspaceindex, lastindex):
+                currentchar = phrase[k]
+                if currentchar == " ":
+                    potentialspaceindex = k + 1
+        if potentialspaceindex == startindex:
+            potentialspaceindex = lastindex
+    for j in range(0,7):
+        line = ""
+        for i in range(startindex,potentialspaceindex):
+            charcurrent = phrase[i]
+            line = line + " " + dictdict[charcurrent][j]
+        print(line,end='\n')
+    if lastindex < len(phrase):
+        if phrase[lastindex] == " ":
+            potentialspaceindex = potentialspaceindex + 1
+    #        print("didthething",end='\n')
+        runtheprint2(potentialspaceindex)
+
+        
+runtheprint2(potentialspaceindex)
+
+    
